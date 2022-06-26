4a55f3b4e9e918f9499cfe04c2ac2ed0f7f5e5c4
Bogdan <bogdan@houseofus.eu>
2022-06-26 13:28:06 +0200

    new test

--- kube_apps/pipelines/extract_yq.sh
+++ kube_apps/pipelines/extract_yq.sh
@@ -3,9 +3,10 @@
 pat=${pat_val}
 myjson=$(curl -u :$pat https://dev.azure.com/bogdan0109/houseofus/_apis/build/builds?definitions=25&resultFilter=succeeded&statusFilter=completed&maxBuildsPerDefinition=1&queryOrder=finishTimeDescending)
 echo $myjson > test.json
-lstbuild_nr=$(jq -r '.value[0].buildNumber' test.json)
-echo "$lstbuild_nr"
-echo "##vso[task.setvariable variable=lastbuildNumber]$lstbuild_nr"
+correct=$(jq -r '[.value[] | {buildNumber,result} | select (.result == "succeeded")] | limit(1;.[]) ' test.json)
+var1=$(jq -r '.buildNumber' <<< $correct)
+echo "var1: $var1"
+echo "##vso[task.setvariable variable=lastbuildNumber]$var1"
 #img=$(yq e '.spec.template.spec.containers[0].image' kube_apps/kuma/deploy-kuma.yaml)
 #echo "img is : $img"
 #echo "##vso[task.setvariable variable=last_buildNumber]$img"
