5871711872a8452585d87b0be29ec0ae536c7e36
Bogdan <bogdan@houseofus.eu>
2022-11-06 10:12:54 +0100

    update new postgres15 removed unused manifests changed conection to db to cluster update pipeline for postgres

--- kube_apps/postgreSQL/template_yq.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-yq e -i '.data.POSTGRES_USER = env(POSTGRES12_USER)' kube_apps/postgreSQL/configmap.yaml
-yq e -i '.data.POSTGRES_PASSWORD = env(POSTGRES12_PASSWORD)' kube_apps/postgreSQL/configmap.yaml
