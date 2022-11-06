3522926c741558751f1005a73cb9789282caedac
Bogdan <bogdan@houseofus.eu>
2022-11-06 10:16:38 +0100

    delete pg 11 manifests

--- kube_apps/postgresv11/template_yq.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-yq e -i '.data.POSTGRES_USER = env(POSTGRES11_USER)' kube_apps/postgresv11/secrets.yaml
-yq e -i '.data.POSTGRES_PASSWORD = env(POSTGRES11_PASSWORD)' kube_apps/postgresv11/secrets.yaml
